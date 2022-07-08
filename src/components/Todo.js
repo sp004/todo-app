93a449d85d947bb45a000316d4ad95249ac8e0a6
sp004 <souvikpal4049@gmail.com>
2022-07-08 18:47:36 +0530

    updated heading in todo

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -6,6 +6,7 @@ import DeleteIcon from '@mui/icons-material/Delete';
 const Todo = ({text}) => {
   return (
     <li className='todo'>
+      <h1>here is the todo list for u</h1>
         <p>{text}</p>
         <EditIcon />
         <DeleteIcon />
